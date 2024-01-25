bfef0881b11529f6c354c22f03de980b05f48c47
Sakshi <sakshibrahmbhatt999@gmail.com>
2024-01-25 21:15:37 +0530

    MQTT Connection done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            Connbtn = new Button();
+            statLbl = new Label();
+            pubBtn = new Button();
+            subBtn = new Button();
+            SuspendLayout();
+            // 
+            // Connbtn
+            // 
+            Connbtn.Location = new Point(31, 13);
+            Connbtn.Name = "Connbtn";
+            Connbtn.Size = new Size(94, 29);
+            Connbtn.TabIndex = 0;
+            Connbtn.Text = "Connect";
+            Connbtn.UseVisualStyleBackColor = true;
+            Connbtn.Click += Connbtn_Click;
+            // 
+            // statLbl
+            // 
+            statLbl.AutoSize = true;
+            statLbl.Location = new Point(131, 17);
+            statLbl.Name = "statLbl";
+            statLbl.Size = new Size(172, 20);
+            statLbl.TabIndex = 1;
+            statLbl.Text = "Not connected to broker";
+            // 
+            // pubBtn
+            // 
+            pubBtn.Location = new Point(31, 60);
+            pubBtn.Name = "pubBtn";
+            pubBtn.Size = new Size(94, 29);
+            pubBtn.TabIndex = 2;
+            pubBtn.Text = "Publish";
+            pubBtn.UseVisualStyleBackColor = true;
+            // 
+            // subBtn
+            // 
+            subBtn.Location = new Point(145, 60);
+            subBtn.Name = "subBtn";
+            subBtn.Size = new Size(94, 29);
+            subBtn.TabIndex = 3;
+            subBtn.Text = "Subscribe";
+            subBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(subBtn);
+            Controls.Add(pubBtn);
+            Controls.Add(statLbl);
+            Controls.Add(Connbtn);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button Connbtn;
+        private Label statLbl;
+        private Button pubBtn;
+        private Button subBtn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,46 @@
+using System;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+
 namespace DataConn
 {
     public partial class Form1 : Form
     {
+        bool connect = false;
+        private MqttClient mqttClient;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Connbtn_Click(object sender, EventArgs e)
+        {
+            if(connect == false)
+            {
+                try
+                {
+                    mqttClient = new MqttClient("www.mqtt-dashboard.com", 1883, false, null, null, MqttSslProtocols.None);
+                    mqttClient.Connect(Guid.NewGuid().ToString());
+                    MessageBox.Show("Connected to MQTT broker!");
+                    Connbtn.Text = "Disconnect";
+                    statLbl.Text = "Connected to broker";
+                    connect = true;
+
+                }
+                catch (MqttConnectionException ex)
+                {
+                    MessageBox.Show("Error connecting to MQTT broker: " + ex.Message);
+                }
+            }
+            else
+            {
+                mqttClient.Disconnect();
+                Connbtn.Text = "Connect";
+                statLbl.Text = "Not Connected to broker";
+                connect = false;
+            }
         }
     }
 }
